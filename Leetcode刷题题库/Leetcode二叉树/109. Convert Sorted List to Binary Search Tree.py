30fbe87655754dd796dc384a49795012b5a13c64
engineerjerry1@gmail.com <engineerjerry1@gmail.com>
2019-12-01 00:20:22 -0800

    update my local code

--- Leetcode刷题题库/Leetcode二叉树/109. Convert Sorted List to Binary Search Tree.py
+++ Leetcode刷题题库/Leetcode二叉树/109. Convert Sorted List to Binary Search Tree.py
@@ -8,3 +8,32 @@
 #    -3   9
 #    /  /
 #  -10  5
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    def sortedListToBST(self, head: ListNode) -> TreeNode:
+        if head is None:  # 边界条件链表的头节点为空，是一个空的二叉树
+            return None
+        if head.next is None:  # 边界条件
+            return TreeNode(head.val)
+
+        prev = None  # 定义一个虚拟节点
+        slow = head  # 定义一个慢指针指向头节点
+        fast = head.next  # 定义一个快指针指向下一个节点
+
+        node = TreeNode(slow.val)  # 根节点
+        node.left = self.sortedListToBST(head)  # 递归place左子树
+        node.right = self.sortedListToBST(slow.next)  # 递归place右子树
+
+        return node  # 循环结束返回根节点
